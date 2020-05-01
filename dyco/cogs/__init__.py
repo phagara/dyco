acd17e954d467a43a970f34a35f4b4b56c424c64
Patrik Hagara <patrihagar@gmail.com>
2020-05-01 15:05:11 +0200

    enable uptime cog

--- dyco/cogs/__init__.py
+++ dyco/cogs/__init__.py
@@ -1,9 +1,11 @@
 from .link_untrack import LinkUntrack
 from .mass_typing import MassTyping
 from .quit import Quit
+from .uptime import Uptime
 
 ALL_COGS = [
     LinkUntrack,
     MassTyping,
     Quit,
+    Uptime,
 ]
