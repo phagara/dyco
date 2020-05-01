565b2b2f67f905e0e85d04caf7c2650b1789fa5a
Patrik Hagara <patrihagar@gmail.com>
2020-05-01 14:52:59 +0200

    quit: message owner on startup

--- dyco/cogs/quit.py
+++ dyco/cogs/quit.py
@@ -2,7 +2,16 @@ from discord.ext import commands
 
 
 class Quit(commands.Cog):
+    def __init__(self, bot: 'commands.Bot'):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        info = await self.bot.application_info()
+        info.owner.send('Dyco (re)started!')
+
     @commands.command()
     @commands.is_owner()
     async def quit(self, ctx):
+        await ctx.send('Shutting down...')
         await ctx.bot.logout()
