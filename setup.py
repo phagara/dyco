17433116eb4cc0b112a19e4cfe41168782d97b76
Patrik Hagara <patrihagar@gmail.com>
2020-05-03 22:32:01 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+import setuptools
+
+
+setuptools.setup(
+    name="dyco",
+    description="a silly discord bot",
+    url="https://github.com/phagara/dyco",
+    version=os.environ.get("DYCO_VERSION"),
+    license="BSD 2-Clause License",
+    platforms=["any"],
+    entry_points={"console_scripts": ["dyco=dyco.__main__:main"]},
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "discord.py",
+        "PyYAML",
+        "urlextract",
+        "humanize",
+        "transliterate",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Environment :: No Input/Output (Daemon)",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Communications :: Chat",
+    ],
+)
